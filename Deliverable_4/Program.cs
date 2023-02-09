f3993668a17c575662e3e1c514c64be6ad2f8d6e
Gabrielle <Gabrielle@LAPTOP-H0ABB48F>
2023-02-09 17:05:45 -0500

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,29 @@
+﻿//Author:Gabby Herrick
+//Date: 2/8/2023
+//Description: Fibonacci spiral d-4
+using System;
+namespace Deliverable_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[25];
+            // Initialize fibonacci series
+            fib[0] = 0;
+            fib[1] = 1;
+            for (int i = 0; i < fib.Length; i++)
+            {
+                if (i >= 2)
+                {
+                    fib[i] = fib[i - 1] + fib  [i - 2];
+                }
+            }
+            // Printing the results in the console
+            for (int i = 0; i < fib.Length; i++)
+            {
+                Console.WriteLine("The Fibonacci for (" + i + ") is equal to " + fib[i]);
+            }
+        }
+    }
+}
